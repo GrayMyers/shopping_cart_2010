22170424da6f5baf228ec5ab036167f76209b103
Grayson Myers <graymyers10@gmail.com>
2020-11-11 09:17:03 -0700

    Add passing test_it_can_check_stock_for_items

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,10 +1,13 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/vendor.rb"
+require "./lib/item.rb"
 
 class Test < Minitest::Test
   def setup
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   def test_it_exists_and_has_attributes
@@ -12,5 +15,9 @@ class Test < Minitest::Test
 
     assert_equal "Rocky Mountain Fresh", @vendor.name
     assert_equal ({}), @vendor.inventory
+  end
+
+  def test_it_can_check_stock_for_items
+    assert_equal 0, @vendor.check_stock(@item1)
   end
 end
