f8479db77c00a08c819cef41919888340bf5d755
Grayson Myers <graymyers10@gmail.com>
2020-11-11 09:08:42 -0700

    Add tests for existence and attributes for item class

---
+++ test/item_test.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item.rb"
+
+class ItemTest < Minitest::Test
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Item, @item1
+
+    assert_equal "Peach", @item1.name
+  end
+
+  def test_it_has_price
+    assert_equal 0.75, @item1.price
+  end
+end
